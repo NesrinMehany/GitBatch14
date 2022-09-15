d2837a19dc918363d25ba1347dc646f1f1288261
NesrinMehany <nesreen.safwat87@gmail.com>
2022-09-14 22:07:42 -0400

    my first commit myself

---
+++ src/gitClasses01/class01code.java
@@ -0,0 +1,10 @@
+package gitClasses01;
+
+public class class01code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is a trial of git");
+	}
+
+}
