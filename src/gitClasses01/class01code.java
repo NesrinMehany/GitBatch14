04016abeb3f87b8299382e8778c510d1ee81aab6
NesrinMehany <nesreen.safwat87@gmail.com>
2022-09-14 22:43:12 -0400

    second commit

--- src/gitClasses01/class01code.java
+++ src/gitClasses01/class01code.java
@@ -5,6 +5,7 @@ public class class01code {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is a trial of git");
+System.out.println("editing my code");
 	}
 
 }
