42fbbc1b9be9990c96ddffdc24a838ec5980fc5e
NesrinMehany <nesreen.safwat87@gmail.com>
2022-09-27 19:28:17 -0400

    simple basic project

---
+++ src/gitClass03/calss03.java
@@ -0,0 +1,10 @@
+package gitClass03;
+
+public class calss03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hey this is a git class");
+	}
+
+}
---
+++ src/gitClasses01/gitClass02.java
@@ -0,0 +1,5 @@
+package gitClasses01;
+
+public class gitClass02 {
+
+}
